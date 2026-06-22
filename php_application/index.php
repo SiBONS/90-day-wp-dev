0e5fd77fe7824bf4a96c6e96e71e9a3972372a29
Mile Kostic <kostic.mile@mail.com>
2026-06-22 20:55:15 +0200

    Day 3-4: Coursera PHP assignment complete

---
+++ php_application/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mile Kostic PHP</title>
+</head>
+<body>
+    <h1>Mile Kostic PHP</h1>
+    <p>The SHA256 of "Mile Kostic" is <?php
+        print hash("sha256", "Charles Severance");
+    ?></p>
+    <pre>
+       <pre>
+    ASCII ART:
+    **    **
+    ***  ***
+    ** ** **
+    **    **
+    **    **
+</pre>
+    </pre>
+   <a href="check.php">Check the settings for this page.</a><br>
+   <a href="fail.php">Click here to cause a traceback.</a>
+</body>
+</html>
