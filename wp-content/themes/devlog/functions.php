f0e6b096040f4f6abf80c71f262a154b99633aab
Mile Kostic <kostic.mile@mail.com>
2026-07-29 16:10:06 +0200

    Rebuilding functions.php with understanding - setup and enqueue done

--- wp-content/themes/devlog/functions.php
+++ wp-content/themes/devlog/functions.php
@@ -9,3 +9,13 @@ function devlog_setup() {
 }
 
 add_action( 'after_setup_theme', 'devlog_setup' );
+
+function devlog_enqueue_assets() {
+   wp_enqueue_style(
+      'devlog-style',
+      get_template_directory_uri() . '/assets/css/style.css',
+      [],
+      '1.0'
+   );
+}
+add_action( 'wp_enqueue_scripts', 'devlog_enqueue_assets' );
